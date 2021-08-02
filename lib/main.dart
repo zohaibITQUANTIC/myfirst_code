1cc2a16b7ee15b8a2058fa43a92072a16585ed15
zohaibITQUANTIC <zohaib.itquantic@gmail.com>
2021-08-02 17:08:03 +0500

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,33 @@ class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My first app"),
-      ),
-      body: Container(
-        child: Center(
+        appBar: AppBar(
+          title: Text("My first app"),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            height: 100,
+            width: 100,
             child: Text(
-                'My first app body where I will make the design for my first app')),
-      ),
-    );
+              'I am a box',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.teal,
+                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey, blurRadius: 5, offset: Offset(2, 5))
+                ]),
+          ),
+        ));
   }
 }
