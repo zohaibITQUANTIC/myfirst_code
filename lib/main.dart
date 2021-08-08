ad11bfeb9276f1ec52be40e9d34d19e774bf38a5
zohaibITQUANTIC <zohaib.itquantic@gmail.com>
2021-08-09 01:40:33 +0500

    ch4 drawer image set state card textfiels

--- lib/main.dart
+++ lib/main.dart
@@ -4,62 +4,100 @@ void main(List<String> args) {
   runApp(MaterialApp(
     title: "My First App",
     home: homepage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  TextEditingController _textCotroller = TextEditingController();
+  var myText = 'Change me';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My first app"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
-              width: 200,
-              height: 400,
-              color: Colors.black,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      color: Colors.red,
-                      padding: const EdgeInsets.all(8),
-                      alignment: Alignment.center,
-                      height: 100,
-                      width: 100,
-                    ),
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text("My first app"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset(
+                  'images/bg.jpg',
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _textCotroller,
+                    decoration: InputDecoration(
+                        labelText: 'Name',
+                        hintText: 'Please enter here',
+                        border: OutlineInputBorder()),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      alignment: Alignment.center,
-                      height: 100,
-                      width: 100,
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      alignment: Alignment.center,
-                      height: 100,
-                      width: 100,
-                      color: Colors.green,
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Zohaib Ahmad'),
+              accountEmail: Text('Example@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Account'),
+              subtitle: Text('Personal'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Mail'),
+              subtitle: Text('forexample@gmail.com'),
+              trailing: Icon(Icons.send),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _textCotroller.text;
+          setState(() {});
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
   }
 }
